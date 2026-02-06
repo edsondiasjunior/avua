95719639e3675370ed1556dfba24a43429eaff2b
1BrandConnect <edson.diascoelho@gmail.com>
2026-02-06 11:39:51 -0300

    update hero

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex items-center justify-between px-4 sm:px-16 py-5">
       <div className=" w-full flex justify-between items-center ">
         <Link href="/" >
-          <Image className="size-22" src={"/avua_logo.svg"} alt="Avuá Agency" height={168} width={116} />
+          <Image className="size-18" src={"/avua_logo.svg"} alt="Avuá Agency" height={168} width={116} />
         </Link>
         <nav>
             
--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -5,9 +5,9 @@ import RevealTitle from "@/components/animation/RevealTitle"
 export default function Hero() {
   return (
     <>
-      <section className="px-8 lg:px-16 py-22 flex flex-col items-center justify-end relative w-full h-[70vh]">
-        <div className="grid grid-cols-12 gap-10 sm:w-full">
+      <section className="px-4 lg:px-16 py-22 flex flex-col items-center justify-end relative w-full h-[70vh]">
 
+        <div className="flex flex-col sm:grid sm:grid-cols-12 gap-10 sm:w-full">
 
           <RevealTitle
             as="h1"
